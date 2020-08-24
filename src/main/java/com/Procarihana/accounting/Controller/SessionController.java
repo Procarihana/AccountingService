ef978111bc0943b0c289f60241ff084f010b2519
Hana <384073877@qq.com>
2020-08-24 22:49:10 +0800

    fix(ci): add forgotten files to fix ci.

---
+++ src/main/java/com/Procarihana/accounting/Controller/SessionController.java
@@ -0,0 +1,35 @@
+package com.procarihana.accounting.controller;
+
+import com.procarihana.accounting.Manager.UserInfoManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1.0/session")
+public class SessionController {
+    private final UserInfoManager userInfoManager;
+
+    @Autowired
+    public SessionController(UserInfoManager userInfoManager) {
+        this.userInfoManager = userInfoManager;
+    }
+
+    /**
+     * User login.
+     * @param username  <b>must not</b> be null
+     * @param password <b>must not</b> be null
+     * @return login success massage.
+     */
+    @PostMapping
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password) {
+
+        userInfoManager.login(username, password);
+        return "success";
+    }
+
+}
